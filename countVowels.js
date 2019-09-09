1b18010f47e46782b288bee2c9b0ca6ddd123554
Shamie <princessshamie@gmail.com>
2019-09-09 13:13:42 +0100

    fix logic

--- countVowels.js
+++ countVowels.js
@@ -1,12 +1,30 @@
-function getVowels(str){
-    var str = 'shamie'
-    var Vowels =str.match(/[aeiou]/gi);
-    var countvowels = Vowels.length;
-    var Vowelstr =Vowels.join('')
+// var countDuplicate = 0
+// countDuplicate++
+// console.log(countDuplicate)
+// console.log(count("The quick brown fox jumps over the lazy dog", 'the'));
 
+
+function getVowels(){
+    var word = 'dahdeh'
+    var Vowels =word.match(/[aeiou]/gi);
+    countDuplicate = 0
+    console.log(countDuplicate)
+    var Duplicate = []
+    for(var x=0; x<word.length; x++){
+
+        if (Duplicate.includes(word[x])){
+            // countDuplicate++
+            this.countDuplicate++
+        }
+        else{
+            Duplicate.push(word[x])
+        }
+    }
+    var Vowelstr =Vowels.join('')
+// var countDuplicate = Duplicate.length
     var arr =[]; 
     arr.push(Vowelstr)
-    arr.push(countvowels)
+    arr.push(countDuplicate)
     console.log(arr)
 }
 getVowels();
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,19 @@
-function fizzbuzz(arr1= [1,2,3,4,5,6] ,arr2= [4,5,6,55,13,14,21,10,7]){
-var arr3 = arr1.concat(arr2)
-if((arr3.length%3)== 0){
-    console.log('Fizz')
-}else if((arr3.length%5)==0){
-    console.log('Buzz')
-}else if(((arr3.length %3) ==0) && (arr3.length% 5)==0){
-    console.log('Fizzbuzz')
-}else
-    console.log(arr3.length)
-}fizzbuzz();
+var array1 = [1,2,3,4,5,6] 
+var array2 = [4,5,6,55]
+
+function fizzbuzz(a, b){
+    var arr3 = a.concat(b)
+    if(((arr3.length%3)==0) && (arr3.length%5)==0){
+        console.log('Fizzbuzz')
+    }
+    else 
+        if((arr3.length%3)==0){
+        console.log('Fizz')
+        }else
+            if((arr3.length% 5)==0){
+                console.log('buzz')
+            }else
+                 console.log(arr3.length)
+    }
+
+fizzbuzz(array1, array2);
